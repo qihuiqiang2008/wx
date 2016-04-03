47348a55b53e9378a1d73bc4ea2854196f4c4360
拂苏 <huiqiang.qhq@alibaba-inc.com>
2016-04-03 10:00:03 +0800

    ..

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var config;
 
 var appidRequired = function (req, res, next) {
   console.log("....");
+  console.log(req.query.s)
   if(req.query.s=="bgu"){
     config={
      token: 'qihuiqiang',
