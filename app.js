aa4ce0ac31ee22fc459ccec80617bfb1dbe162a6
拂苏 <huiqiang.qhq@alibaba-inc.com>
2016-04-03 17:43:18 +0800

    ..

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use('/wechat',appidRequired,wechat(config, function (req, res, next) {
     res.reply("输入格式错误，请回复学校+名字的形式，例如:清华大学 王晓春")
     return;
  }
- if(!(schoolnameMap[req.weixin.Content.split(" ")[0])){
+ if(!(schoolnameMap[req.weixin.Content.split(" ")[0]])){
    res.reply("没有找到相关表白，请努力呀")；
    return；
  }
